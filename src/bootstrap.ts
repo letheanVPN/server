8578f2f0ac3889480db223a02713ba30eed9b850
Snider <snider@lt.hn>
2025-01-31 20:24:46 +0000

    update version in deno.json and modify Swagger API details

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -4,17 +4,23 @@ import { AppModule } from './app.module.ts';
 import { DanetApplication } from '@danet/core';
 import { loggerMiddleware } from './logger.middleware.ts';
 import { SpecBuilder, SwaggerModule } from '@danet/swagger';
+import * as version from "../deno.json" with { type: "json" };
+
+
 export const bootstrap = async () => {
   const application = new DanetApplication();
+  application.enableCors();
   await application.init(AppModule);
 
-  const staticAssetsPath = `${dirname(import.meta.dirname?import.meta.dirname:Deno.cwd())}/include`;
+  const staticAssetsPath = `${dirname(import.meta.dirname ? import.meta.dirname : Deno.cwd())}/include`;
   application.useStaticAssets(staticAssetsPath);
+
+
   const spec = new SpecBuilder()
-    .setTitle('Todo')
-    .setDescription('The todo API')
-    .setVersion('1.0')
-    .build();
+      .setTitle('Lethean Server')
+      .setDescription('Multi use server for Lethean')
+      .setVersion(version.default.version)
+      .build();
   const document = await SwaggerModule.createDocument(application, spec);
   await SwaggerModule.setup('api', application, document);
   application.addGlobalMiddlewares(loggerMiddleware);
