faebbe820ec59f89b53dcf2809d6b1835b1a6225
Snider <snider@lt.hn>
2025-02-01 15:12:53 +0000

    Add platform-specific executable extension handling in ProcessService

--- src/process/controller.ts
+++ src/process/controller.ts
@@ -14,50 +14,32 @@ import {ProcessService} from "./service.ts";
 export class ProcessController  {
     constructor(private process: ProcessService) {}
 
-    /**
-     * Run a src
-     */
-    @Post("run")
-    async runProcess(@Body() body: ProcessRegister) {
-        const { code, stdout, stderr } = await this.process.run(body.command);
-        return { code, out: new TextDecoder().decode(stdout), error: new TextDecoder().decode(stderr) };
-    }
+    // @Post("run")
+    // async runProcess(@Body() body: ProcessRegister) {
+    //     const { code, stdout, stderr } = await this.process.run(body.command);
+    //     return { code, out: new TextDecoder().decode(stdout), error: new TextDecoder().decode(stderr) };
+    // }
+
+    // @Post("add")
+    // addProcess(@Body() body: ProcessRegister) {
+    //     return this.process.add(body.key, [body.command]);
+    // }
+
+    // @Post("start")
+    // startProcess(@Body() body: ProcessRequestStart) {
+    //     return this.process.startProcess(body.key);
+    // }
+
+    // @Post("stop")
+    // stopProcess(@Body() body: ProcessRequestStop) {
+    //     return this.process.stopProcess(body.key);
+    // }
 
 
-    /**
-     * Add a src to src registry
-     */
-    @Post("add")
-    addProcess(@Body() body: ProcessRegister) {
-        return this.process.add(body.key, [body.command]);
-    }
-
-
-    /**
-     * Start a src in the src registry
-     */
-    @Post("start")
-    startProcess(@Body() body: ProcessRequestStart) {
-        return this.process.startProcess(body.key);
-    }
-
-    /**
-     * Stop a src in the src registry
-     */
-    @Post("stop")
-    stopProcess(@Body() body: ProcessRequestStop) {
-        return this.process.stopProcess(body.key);
-    }
-
-
-    /**
-     * Kill a src in the src registry
-     * @param {ProcessKillDTO} body
-     */
-    @Post("kill")
-    killProcess(@Body() body: ProcessRequestRemove) {
-        return this.process.kill(body.key);
-    }
+    // @Post("kill")
+    // killProcess(@Body() body: ProcessRequestRemove) {
+    //     return this.process.kill(body.key);
+    // }
 
 
 
