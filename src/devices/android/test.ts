30643692b0bf8c19942ae942c94b1144bdd6ec4c
Snider <snider@lt.hn>
2025-02-07 22:54:22 +0000

    Android Debug Bridge Support

--- src/devices/android/test.ts
+++ src/devices/android/test.ts
@@ -2,8 +2,9 @@ import {DevicesAndroidService} from "./service.ts";
 import { join } from "@std/path/join";
 import { assertEquals } from '@std/assert';
 const androidService = new DevicesAndroidService();
+await androidService.downloadADB(Deno.cwd())
 Deno.test('Android - Fetch Win/Lin/Mac ADB', async () => {
-    await androidService.downloadADB(Deno.cwd())
+    // await androidService.downloadADB(Deno.cwd())
     if(Deno.build.os === "windows"){
         assertEquals(Deno.statSync(join(Deno.cwd(), "platform-tools", "adb.exe")).isFile, true, "ADB should be downloaded");
     }else{
